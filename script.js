e7cea418387881256385363eb9b03142d542fcae
Faisaljabrah <>
2021-06-03 13:45:23 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let number = 0
+let sentence = "hello i'm 16."
+
+document.getElementById("button").addEventListener("click",addition)
+function () {
+number = document.getElementById("input").value 
+number = parseInt(number)
+
+number = number + 1
+alert(number)
+
+}
